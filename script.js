471979bf4477ed1e2b27847d8f277d574e71e5b0
Ayuna <ashagzhina@gmail.com>
2019-03-01 13:07:49 +0800

    new project

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strit'
+
+const fs = require('fs');
+const text = fs.readFileSync('script.js', 'utf8');
+console.log(text);
---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict'
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function(request, response){
+    console.log(request.method, request.url);
+    if(request.url == '/style.css'){
+        const css = fs.readFileSync('style.css', 'utf8');
+        response.end(css)
+    } 
+    if(request.url == '/bd.jpg'){
+        const jp = fs.readFileSync('bd.jpg');
+        response.end(jp)
+    } 
+    if(request.url == '/fonts/13218.otf'){
+        const jp = fs.readFileSync('fonts/13218.otf');
+        response.end(jp)
+    } 
+    if(request.url == '/fonts/14651.ttf'){
+        const jp = fs.readFileSync('fonts/14651.ttf');
+        response.end(jp)
+    } 
+    else {
+        const text = fs.readFileSync('index.html', 'utf8');
+        response.end(text);
+    }
+});
+
+server.listen(1000);
+console.log("Server started!")
